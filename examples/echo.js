cbd6d2da038823eb3dd23633cd862cf74433c3d0
David Brown <github@scri.pt>
2013-04-12 13:34:04 -0700

    A bit of tidying along error paths.

--- examples/echo.js
+++ examples/echo.js
@@ -1,14 +1,23 @@
 
 var gammu = require('node-gammu-json');
-var server = gammu.create({ interval: 0 });
+var server = gammu.create({ interval: 2 });
 
 server.on({
+
   receive: function (_message, _callback) {
-    this.send(_message.from, _message.content, function (_m, _result) {
-      console.log('transmit:', _result);
+    this.send(_message.from, _message.content, function (_e, _m, _r) {
+      console.log('transmit callback:', _e, _r);
     });
     console.log('receive:', _message);
     _callback();
+  },
+
+  transmit: function (_message, _result) {
+    console.log('transmit:', _message, _result);
+  },
+
+  error: function (_error, _message) {
+    console.log('error:', _error, _message);
   }
 }).start();
 
